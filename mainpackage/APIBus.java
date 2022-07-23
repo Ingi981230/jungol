3859693d8e0f76f3e9678c7e9b5d7ed4b332fe21
Ingi981230 <107035012+Ingi981230@users.noreply.github.com>
2022-07-23 15:19:48 +0900

    Update APIBus.java

--- mainpackage/APIBus.java
+++ mainpackage/APIBus.java
@@ -9,14 +9,14 @@ import java.io.IOException;
 public class APIBus {
     public static void main(String[] args) throws IOException {
         StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1613000/ExpBusInfoService/getStrtpntAlocFndExpbusInfo"); /*URL*/
-        urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=c6Pq%2F5ys7G3CEHptRvGp5IdyZ3sqigWa34YzkFpogmqeKbyK3JEkAkC7lQLOzYN5wjYcnZARm09A7biz1RErhQ%3D%3D"); /*Service Key*/
-        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*ÆäÀÌÁö¹øÈ£*/
-        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*ÇÑ ÆäÀÌÁö °á°ú ¼ö*/
-        urlBuilder.append("&" + URLEncoder.encode("_type","UTF-8") + "=" + URLEncoder.encode("xml", "UTF-8")); /*µ¥ÀÌÅÍ Å¸ÀÔ(xml, json)*/
-        urlBuilder.append("&" + URLEncoder.encode("depTerminalId","UTF-8") + "=" + URLEncoder.encode("NAEK010", "UTF-8")); /*Ãâ¹ßÅÍ¹Ì³ÎID*/
-        urlBuilder.append("&" + URLEncoder.encode("arrTerminalId","UTF-8") + "=" + URLEncoder.encode("NAEK300", "UTF-8")); /*µµÂøÅÍ¹Ì³ÎID*/
-        urlBuilder.append("&" + URLEncoder.encode("depPlandTime","UTF-8") + "=" + URLEncoder.encode("20211201", "UTF-8")); /*Ãâ¹ßÀÏ(YYYYMMDD)*/
-        urlBuilder.append("&" + URLEncoder.encode("busGradeId","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*¹ö½ºµî±Ş*/
+        urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=ì„œë¹„ìŠ¤í‚¤ë¥¼ ì…ë ¥í•˜ì„¸ìš”"); /*Service Key*/
+        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*í˜ì´ì§€ë²ˆí˜¸*/
+        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*í•œ í˜ì´ì§€ ê²°ê³¼ ìˆ˜*/
+        urlBuilder.append("&" + URLEncoder.encode("_type","UTF-8") + "=" + URLEncoder.encode("xml", "UTF-8")); /*ë°ì´í„° íƒ€ì…(xml, json)*/
+        urlBuilder.append("&" + URLEncoder.encode("depTerminalId","UTF-8") + "=" + URLEncoder.encode("NAEK010", "UTF-8")); /*ì¶œë°œí„°ë¯¸ë„ID*/
+        urlBuilder.append("&" + URLEncoder.encode("arrTerminalId","UTF-8") + "=" + URLEncoder.encode("NAEK300", "UTF-8")); /*ë„ì°©í„°ë¯¸ë„ID*/
+        urlBuilder.append("&" + URLEncoder.encode("depPlandTime","UTF-8") + "=" + URLEncoder.encode("20211201", "UTF-8")); /*ì¶œë°œì¼(YYYYMMDD)*/
+        urlBuilder.append("&" + URLEncoder.encode("busGradeId","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*ë²„ìŠ¤ë“±ê¸‰*/
         URL url = new URL(urlBuilder.toString());
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
